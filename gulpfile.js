79f80459bc91d56fe4c1afcfaf9688a9280f7b68
Simon Dean <simon@simondean.org>
2015-05-01 23:25:44 +0100

    Automate releases

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,53 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var del = require('del');
+var changed = require('gulp-changed');
 var react = require('gulp-react');
+var bump = require('gulp-bump');
+var spawn = require('child_process').spawn;
 
 var paths = {
-  components: 'components/**/*.jsx'
+  components: 'components/**/*.jsx',
+  build: 'build'
 };
+
+gulp.task('clean', function(done) {
+  del([paths.build], done);
+});
 
 gulp.task('transpile', function() {
   return gulp.src(paths.components)
+    .pipe(changed(paths.build))
     .pipe(react({harmony: true}))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest(paths.build));
 });
 
 gulp.task('watch', function() {
   gulp.watch(paths.components, ['transpile']);
 });
 
+gulp.task('build', function(done) {
+  runSequence(
+    'clean',
+    ['transpile'],
+    done);
+});
 
-gulp.task('default', ['watch', 'transpile']);
+gulp.task('bump', function(){
+  gulp.src('package.json')
+    .pipe(bump())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('npm-publish', function (done) {
+  spawn('npm', ['publish'], { stdio: 'inherit' }).on('close', done);
+});
+
+gulp.task('release', function(done) {
+  runSequence(
+    'build',
+    'npm-publish',
+    done);
+});
+
+gulp.task('default', ['watch', 'build']);
